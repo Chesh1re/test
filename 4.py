0758fc002c38b0171626589b16df95663013413f
Chesh1re <419866853@qq.com>
2016-09-05 21:20:47 +0800

    9.5 21:20

--- 4.py
+++ 4.py
@@ -6,7 +6,7 @@ def fff(x):
 	if x>3:
 		return True
 print filter(fff,l)
-
+print filter(lambda x:x>3,l)
 a=[1,2,3]
 b=[3,4,5]
 print zip(a,b)
@@ -15,3 +15,7 @@ print zip(a,b,c)
 print map(None,a,b)
 print map(None,a,b,c)
 print map(ff,a,b)
+print [x*2 for x in l]
+print [x*y for x in range(10) if x>3 if x%2 for y in range(5) if y%3]
+
+print [x*y for x in range(10) if x>3 if x%2 for y in range(5) if not (y%3)]
