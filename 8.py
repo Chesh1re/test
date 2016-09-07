d98dd1a9e6b87d8a84a40523cb8964ecffea6bac
Chesh1re <419866853@qq.com>
2016-09-07 18:28:14 +0800

    9.7 18:28

--- 8.py
+++ 8.py
@@ -14,7 +14,7 @@ f1=open('tes,txt','w+')
 f1.write("abc")
 print f1.read()
 print f1.read()
-f1.flush()
+f1.flush()   #update
 l= ['one\n','two\n','three\n']
 f1.writelines(l)
 
@@ -24,4 +24,10 @@ print f1.read()
 f0.close()
 f1.close()
 #seek(offset,opt)  opt:0 head    1 current  2 end   
+
+f3=file('dd.txt','a+')
+f3.write("45634562345")
+f3.seek(0,0)
+print f3.read()
+f3.close()
 
