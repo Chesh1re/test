87f4aa3131d5b2fb7cc93c1107f9cfa6682bcad1
Chesh1re <419866853@qq.com>
2016-09-08 22:07:12 +0800

    22:07

---
+++ 12.py
@@ -0,0 +1,22 @@
+>>> import MySQLdb
+>>> conn = MySQLdb.connect(user='root',passwd='admin',host='127.0.0.1')
+>>> cur= conn.cursor()
+>>> cur.execute("insert into new_table values('tom','002')")
+>>> sql = "insert into new_table values(%s,%s)"
+>>> cur.execute(sql,('tomy','003'))
+>>> cur.execute("select * from new_table")
+executemany(sql,[(),(),()])
+delete from ... where
+update ... set name='' where ..
+>>> cur.fetchone()
+>>> cur.fetchmany()
+()
+>>> cur.scroll(0,"absolute")
+>>> cur.fetchmany()
+(('amy', '001'),)
+>>> cur.fetchmany(3)
+(('tom', '002'), ('tomy', '003'))
+>>>
+>>> cur.close()
+>>> conn.close()
+
